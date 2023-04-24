6b6877efacc1370db32d9c6efafbcd3bc941b2b1
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-04-24 12:08:56 +0300

    complete pagination

--- controllers/getAll.js
+++ controllers/getAll.js
@@ -1,18 +1,9 @@
 const { Contact } = require("../models/contact");
 const paginate = require("mongoose-paginate-v2");
 
-// const getAll = async (req, res) => {
-//   const { _id: owner } = req.user;
-//   const result = await Contact.find(
-//     { owner },
-//     "-createdAt -updatedAt"
-//   ).populate("owner", "name email");
-//   res.json(result);
-// };
-
 const getAll = async (req, res) => {
   const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 3;
+  const limit = parseInt(req.query.limit) || 20;
   const favorite = req.query.favorite;
   const filter = {};
   const options = {
@@ -24,8 +15,9 @@ const getAll = async (req, res) => {
   if (favorite !== undefined) {
     filter.favorite = favorite;
   }
-  let contacts = await Contact.find(filter);
-  contacts = await Contact.paginate({}, options);
+
+  const contacts = await Contact.paginate(filter, options);
+
   res.json(contacts);
 };
 module.exports = getAll;
